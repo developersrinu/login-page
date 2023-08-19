73165253ec5b96f78e01e53e98b17224ab06122c
developersrinu <orsusrinu606@gmail.com>
2023-08-19 19:20:29 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Axios } from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      let [userName, setUserName] = useState('')
+      let [password, setPassword] = useState('')
+      let [error, setError] = useState('Forget your Password')
+
+      function handleClick(e) {
+            e.preventDefault();
+
+      
+
+
+
+      }
+
+      return (
+            <div className='container'>
+                  <form onSubmit={handleClick}>
+                        <p className='p1'>Welcome Back<span className='s1' >👋</span></p>
+
+                        <h2>Sign in to your account</h2>
+                        <label htmlFor='input1' >Your Name</label>
+                        <input
+                              type='text'
+                              id='input1'
+                              onChange={(e) => setUserName(e.target.value)}
+                              value={userName}
+                        />
+
+
+                        <label htmlFor='input2'>Password</label>
+                        <input type='password' id='input2' onChange={(e) => setPassword(e.target.value) } value={password} />
+
+                        <input type='submit' placeholder='CONTINUE' id='btn' />
+                        <p id='pp'>{error}</p>
+                  </form>
+
+                  <p id='pr'>Don't have an account?<span>Sign Up</span></p>
+
+
+
+
+            </div>
+      );
 }
 
 export default App;
+
+
+
+
